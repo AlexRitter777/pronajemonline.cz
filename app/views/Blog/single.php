70f05b87fa92078c41d26c1a1036d1aa63b2c66a
Alex <bogachev@email.cz>
2025-08-06 12:12:56 +0200

    Version 2.30.2 - Fixed bug with post creation date in the single post public view

--- app/views/Blog/single.php
+++ app/views/Blog/single.php
@@ -7,7 +7,7 @@
                         <a href="/blog">Blog</a> / <a href="/blog/category/<?= $post->category->slug; ?>"><?= $post->category->title; ?></a> / <?= $post->title;?>
                 </span>
                 <div class="post-meta">
-                    <span class="post-date"><?= date('d.m.Y', $post->crrated_at);?></span>
+                    <span class="post-date"><?= date('d.m.Y', strtotime($post->created_at));?></span>
                 </div>
             </div>
             <?php require_once APP . "/views/Includes/single_post.php"; ?>
