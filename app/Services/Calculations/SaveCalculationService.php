0b1e08bc32d094ba08e92cffdd0dd55be18e2a0b
Alex <bogachev@email.cz>
2026-09-25 10:34:10 +0200

    feat(services): add SaveCalculationService for saving calculation data

---
+++ app/Services/Calculations/SaveCalculationService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Services\Calculations;
+
+
+use app\Models\Servicescalc;
+use DI\Attribute\Inject;
+
+final class SaveCalculationService
+{
+
+    #[Inject]
+    private readonly Servicescalc $servicescalc;
+
+    public function saveCalculation(array $data, float $result, int $userId)
+    {
+
+        $prepared = $this->prepareData($data);
+
+        $prepared['created_at'] = date('Y-m-d H:i:s');
+        $prepared['updated_at'] = date('Y-m-d H:i:s');
+        $prepared['result'] = $result;
+        $prepared['user_id'] = $userId;
+
+        return $this->servicescalc->saveAll($prepared);
+
+    }
+
+    private function prepareData(array $data)
+    {
+        foreach ($data as $key => $value) {
+            if($value !== 'Ano' && $value !== 'Ne'){
+
+                if (is_array($value)){
+                    $value = "^" . implode("^", $value);
+                }
+
+                $data[$key] = $value;
+
+            }
+        }
+
+        return $data;
+    }
+
+}
